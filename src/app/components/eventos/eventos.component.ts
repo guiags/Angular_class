66f1baf328d2ab75759838bfa960f65783ecc029
unknown <guilhermeaugustogs2999@gmail.com>
2023-03-09 17:49:06 -0300

    sec commit

---
+++ src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent {
+  showMessage : boolean = true;
+  
+  
+  Unshow(): void {
+    /*if(this.showMessage == false){
+      this.showMessage = true;
+    }else{
+      this.showMessage = false;
+    }*/
+    this.showMessage = !this.showMessage;
+  }
+  
+
+}
